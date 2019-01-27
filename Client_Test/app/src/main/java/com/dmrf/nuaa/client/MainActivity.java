1a3b44ab63f61bd0c56c77708748f7dbb048c14a
Rosyphoton <814290660@qq.com>
2019-01-27 17:27:07 +0100

    first commit

---
+++ Client_Test/app/src/main/java/com/dmrf/nuaa/client/MainActivity.java
@@ -0,0 +1,226 @@
+package com.dmrf.nuaa.client;
+
+import android.nfc.Tag;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button ok;
+    private Button upload;
+    private Button read;
+    private EditText username;
+    private EditText password;
+
+    OkHttpClient client = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ok=findViewById(R.id.ok);
+        upload=findViewById(R.id.upload);
+        read=findViewById(R.id.read);
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        final String base_url = "http://192.168.1.13/";
+
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name= String.valueOf(username.getText());
+                String pass= String.valueOf(password.getText());
+                String url1 = base_url + "register";
+                SendMessage(url1,name,pass);
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String filepath = Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera/image_demo.jpg";
+                String url2 = base_url + "upload";
+                SendFile(url2, filepath);
+            }
+        });
+
+        read.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String filepath = Environment.getExternalStorageDirectory().toString() + "/TEST/humans.txt";
+                readFile(filepath);
+            }
+        });
+    }
+
+    private void readFile(String filepath)
+    {
+        if (filepath == null) return;
+
+        File file = new File(filepath);
+        if (file.isDirectory()){
+            Toast.makeText(MainActivity.this, filepath + "is Directory", Toast.LENGTH_SHORT).show();
+            return;
+        }else {
+            try {
+                InputStream is = new FileInputStream(file);
+                if (is != null) {
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);
+
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        Toast.makeText(MainActivity.this, line, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                Toast.makeText(MainActivity.this, filepath + " dosen't found", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                Toast.makeText(MainActivity.this, filepath + "read exception,"+ e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void SendFile(String url, String filepath)
+    {
+        MediaType Image = MediaType.parse("image/jpeg; charset=utf-8");
+        File file = new File(filepath);
+        RequestBody body = RequestBody.create(Image, file);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Server Wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String res = response.body().string();
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        if (res.equals("0"))
+                        {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this,"already registered",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                        else
+                        {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this,"succeed",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void SendMessage(String url,final String userName,String passWord)
+    {
+        FormBody.Builder formBuilder = new FormBody.Builder();
+        formBuilder.add("username", userName);
+        formBuilder.add("password", passWord);
+        Request request = new Request.Builder().url(url).post(formBuilder.build()).build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback()
+        {
+            @Override
+            public void onFailure(Call call, IOException e)
+            {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this,"Server Wrong",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException
+            {
+                final String res = response.body().string();
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        if (res.equals("0"))
+                        {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this,"already registered",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                        else
+                        {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this,"succeed",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+
+                    }
+                });
+            }
+        });
+
+    }
+}
---
+++ Server/Server.py
@@ -0,0 +1,60 @@
+#-*-coding:utf-8-*-
+from flask import Flask
+from flask import request, make_response, redirect
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+basedir=os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.config['UPLOAD_FOLDER'] = 'static\\uploads'
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'txt', 'jpg'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def test():
+    return '服务器正常运行'
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    upload_file = request.files['file']
+    if upload_file and allowed_file(upload_file.filename):
+        filename = secure_filename(upload_file.filename)
+        upload_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], filename))
+        return 'hello,successful'
+    else:
+        return 'hello,failed'
+
+@app.route('/show/<string:filename>', methods=['GET'])
+def show_photo(filename):
+    file_dir = os.path.join(basedir, app.config['UPLOAD_FOLDER'])
+    # filename = "111.jpg"
+    if request.method == 'GET':
+        if filename is None:
+            pass
+        else:
+            image_data = open(os.path.join(file_dir, '%s' % filename), "rb").read()
+            response = make_response(image_data)
+            response.headers['Content-Type'] = 'image/jpg'
+            return response
+    else:
+        pass
+
+#此方法处理用户注册
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username=request.form['username']
+        password=request.form['password']
+
+        print('username:'+username)
+        print('password:'+password)
+        return 'register successfully'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
